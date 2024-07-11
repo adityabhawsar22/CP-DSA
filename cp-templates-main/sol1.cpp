80c56409e542a25883efd371380ff769e25e1bcd
adityabhawsar22 <111380717+adityabhawsar22@users.noreply.github.com>
2024-07-12 01:50:57 +0530

    Add files via upload

---
+++ cp-templates-main/sol1.cpp
@@ -0,0 +1,63 @@
+   #include<bits/stdc++.h>
+   #ifndef ONLINE_JUDGE
+   #include "debug.h"
+   #else
+   #define debug(x)  
+   #endif
+
+   using namespace std;
+
+   #define MOD 1000000007
+   #define MOD1 998244353
+   #define INF 1e18
+   #define nline "\n"
+   #define pb push_back
+   #define ppb pop_back
+   #define mp make_pair
+   #define ff first
+   #define ss second
+   #define sz(x) ((int)(x).size())
+   #define all(x) (x).begin(), (x).end()
+   #define for0(i,x) for(int i=0;i<x;i++)
+   #define for1(i,x) for(int i=1;i<=x;i++) 
+   #define forR(i,x) for(int i=x-1;i>=0;i--)
+
+   typedef long long ll;
+   typedef vector<int> vi;
+   typedef pair<int,int> pi;
+
+
+
+   void solve()
+   {
+      
+      
+      int tt;
+      cin >> tt;
+      while(tt--)
+      {
+         debug(tt)
+         
+      }
+      
+
+   }
+
+
+   int main()
+   {
+      #ifndef ONLINE_JUDGE
+         freopen("Error.txt", "w", stderr);
+         freopen("input.txt", "r", stdin);
+         freopen("output.txt", "w", stdout);
+      #endif
+      ios_base::sync_with_stdio(false);
+      cin.tie(NULL);
+      cout.tie(NULL);
+      
+      solve();
+      return 0;
+    
+   } 
+
+
